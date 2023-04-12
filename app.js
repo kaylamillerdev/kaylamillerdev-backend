0e9868eae3f63509af5449b0138164e5f057152a
Kayla Miller <kayla@kaylamiller.dev>
2023-04-12 10:51:15 -0400

    more tweaking

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use(function(err, req, res, next) {
 
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  //console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors');
 Project = require('../models/project.model');
 Bio = require('../models/aboutMe.model');
 
